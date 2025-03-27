1543b0bd5a771eb4f68c6d9e1989fd8395972f24
marcosnunes <marcos.nunes.lph@outlook.com>
2025-03-26 23:44:13 -0300

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   plugins: [
     new CopyPlugin({
       patterns: [
-        { from: 'node_modules/pdfjs-dist/build/pdf.worker.mjs', to: 'pdf.worker.js' },
+        { from: 'node_modules/pdfjs-dist/build/pdf.worker.mjs', to: 'pdf.worker.mjs' },
       ],
     }),
   ],
